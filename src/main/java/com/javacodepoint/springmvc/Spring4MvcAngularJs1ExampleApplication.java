7d559012d1a1357c92e9d0abd89062577e017e7d
Ravi Kumar <Ravi.Kumar@a1technology.local>
2017-07-08 07:36:49 +0530

    1st commit

---
+++ src/main/java/com/javacodepoint/springmvc/Spring4MvcAngularJs1ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.javacodepoint.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4MvcAngularJs1ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4MvcAngularJs1ExampleApplication.class, args);
+	}
+}
